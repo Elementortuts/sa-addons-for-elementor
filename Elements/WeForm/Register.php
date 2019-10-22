8bf848e142a851e308ecc171df3b4acb839fcd2a
jabir88 <42732866+jabir88@users.noreply.github.com>
2019-10-22 15:33:23 +0600

    caldera and weform

---
+++ Elements/WeForm/Register.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'name' => 'WeForm',
+    'class' => '\SA_EL_ADDONS\Elements\WeForm\WeForm',
+    'dependency' => [
+      
+    ],
+    'category' => 'Form Elements',
+    'Premium' => TRUE,
+    'condition' => '',
+    'API' => ''
+];
