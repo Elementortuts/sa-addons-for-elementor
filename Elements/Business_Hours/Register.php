c8067f81033d9ac9b54aea54da1849b8adb44909
jabir88 <42732866+jabir88@users.noreply.github.com>
2019-10-26 09:04:13 +0600

    Business

---
+++ Elements/Business_Hours/Register.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'name' => 'Business_Hours',
+    'class' => '\SA_EL_ADDONS\Elements\Business_Hours\Business_Hours',
+    'dependency' => [
+        'css' => [
+            SA_EL_ADDONS_PATH . 'Elements/Business_Hours/assets/index.min.css',
+        ],
+    ],
+    'category' => 'Marketing Elements',
+    'Premium' => TRUE,
+    'condition' => '',
+    'API' => ''
+];
