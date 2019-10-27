5a681366d47be66c6a9424d6c5169fc04635e1b3
jabir88 <42732866+jabir88@users.noreply.github.com>
2019-10-27 12:39:15 +0600

    rec

--- Elements/Recipe/Register.php
+++ Elements/Recipe/Register.php
@@ -7,10 +7,6 @@ return [
         'css' => [
             SA_EL_ADDONS_PATH . 'Elements/Recipe/assets/index.min.css',
         ],
-        'js' => [
-
-            SA_EL_ADDONS_PATH . 'Elements/Recipe/assets/index.min.js',
-        ],
     ],
     'category' => 'Creative Elements',
     'Premium' => FALSE,
