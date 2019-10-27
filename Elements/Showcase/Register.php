009dd90224210693d22ef31d2d1205177622aa11
jabir88 <42732866+jabir88@users.noreply.github.com>
2019-10-27 11:11:17 +0600

    showcase

---
+++ Elements/Showcase/Register.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'name' => 'Showcase',
+    'class' => '\SA_EL_ADDONS\Elements\Showcase\Showcase',
+    'dependency' => [
+        'css' => [
+            SA_EL_ADDONS_PATH . 'assets/vendor/fancybox/css/jquery.fancybox.min.css',
+            SA_EL_ADDONS_PATH . 'Elements/Showcase/assets/index.min.css',
+        ],
+        'js' => [
+            SA_EL_ADDONS_PATH . 'assets/vendor/fancybox/js/jquery.fancybox.min.js',
+            SA_EL_ADDONS_PATH . 'assets/vendor/jquery-resize/js/jquery.resize.min.js',
+            SA_EL_ADDONS_PATH . 'Elements/Showcase/assets/index.min.js',
+        ],
+    ],
+    'category' => 'Creative Elements',
+    'Premium' => FALSE,
+    'condition' => '',
+    'API' => ''
+];
